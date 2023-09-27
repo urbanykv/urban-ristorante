37f0fc888dc74d7b0e3d395393c18fc4655aa702
urbanykv <matheusurban19@gmail.com>
2023-09-27 08:34:19 -0300

    urban ristorante

--- src/App.js
+++ src/App.js
@@ -5,22 +5,30 @@ import produtos from './data-produtos';
 
 function App() {
 
-  const entradas = produtos.filter(produto => 
+  const entradas = produtos.filter( produto => 
     produto.categoria === 'Entradas')
 
   const [itensCardapio, setItensCardapio] = useState(entradas);
-  const [buscarItemCardapio, setBuscarItemCardapio] = useState();
+  const [textoBusca, setTextoBusca] = useState("");
 
-  const filtrarProdutos = (categoria) => {
-    setItensCardapio(produtos.filter(produto => 
+  const filtrarProdutos = categoria => {
+    setItensCardapio(produtos.filter( produto => 
       produto.categoria === categoria
     ))
+    setTextoBusca("")
+  }
+
+  const buscarItem = text => {
+    setTextoBusca(text)
+    setItensCardapio(produtos.filter( produto => 
+      produto.nome.toLowerCase().includes(text.toLowerCase())
+      ))
   }
 
   return (
     <div className="App">
       <header className='header'>
-        <h1>Restaurant</h1>
+        <h1>Urban's Ristorante</h1>
         <p>De pratos clássicos a criações surpreendentes, nosso cardápio é um requinte de sabores refinados.</p>
       </header>
       <main>
@@ -33,7 +41,7 @@ function App() {
             <button className='btns' onClick={() => filtrarProdutos('Saladas')}><img src='assets/salada.png' alt='Saladas Icon'/>Saladas</button>
             <button className='btns' onClick={() => filtrarProdutos('Sobremesas')}><img src='assets/sobremesa.png' alt='Sobremesas Icon'/>Sobremesas</button>
           </nav>
-          <input type='text' id='buscaMenu' onChange={} placeholder='Pesquise aqui um dos pratos do nosso cardápio'/>
+          <input type='text' id='buscaMenu' value={textoBusca} onChange={(event) => buscarItem(event.target.value)} placeholder='Pesquise aqui um dos pratos do nosso cardápio'/>
         </section>
         <section className='listagem-cardapio'>
           <h2>Cardápio</h2>
